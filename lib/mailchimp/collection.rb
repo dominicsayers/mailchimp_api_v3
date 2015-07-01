da4d93425cadd3c81575d5669960922c3f8e71fb
Dominic Sayers <dominic@sayers.cc>
2015-07-01 11:14:31 +0100

    WIP

--- lib/mailchimp/collection.rb
+++ lib/mailchimp/collection.rb
@@ -1,15 +1,22 @@
 module Mailchimp
   module Collection
+    def self.included(base)
+      puts "#{self} included in #{base}" # debug
+    end
+
     def initialize(client, parent_path)
       @client = client
       @parent_path = parent_path
+      puts "#{self} initialized" # debug
+      puts self.class.const_get :PATH_KEY # debug
+      puts self.class::PATH_KEY # debug
 
-      first_page = @client.get(path)[DATA_KEY].map { |d| CHILD_CLASS.new @client, path, d }
-      super first_page
+      # first_page = @client.get(path)[DATA_KEY].map { |d| CHILD_CLASS.new @client, path, d }
+      # super first_page
     end
 
     def path
-      @path ||= "#{@parent_path}/#{PATH_KEY}"
+      # @path ||= "#{@parent_path}/#{PATH_KEY}"
     end
   end
 end
